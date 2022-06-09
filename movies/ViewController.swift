625d15fa2ed3c5b6df9e2106f56d53f46f393850
jihoi-kang <jihoi.kang@gmail.com>
2022-06-09 14:14:00 +0900

    Initial Commit

---
+++ movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movies
+//
+//  Created by Jihoi Kang on 2022/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
